560b17f52be114cf71a8908b6af2dd90b5c60fa7
Andrea Pantieri <andrea.pantieri98@gmail.com>
2020-12-02 23:03:35 +0100

    new Entities

---
+++ MuseumsManager/Entities/Museo_Provenienza.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+    public class Museo_Provenienza
+    {
+        public int idMuseo;
+        public int idProvenienza;
+    }
+}
