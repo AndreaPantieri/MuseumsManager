98d9c620ef6e5475a64282eec301361cb17550e5
Andrea Pantieri <andrea.pantieri98@gmail.com>
2020-12-02 17:01:22 +0100

    add entities

---
+++ MuseumsManager/Entities/DBConnection.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+    public class DBConnection
+    {
+        private static readonly string ConnectionString = "Data Source=LAPTOP-A2UM0TN5;Initial Catalog=MuseumsManagerDB;Integrated Security=True";
+
+
+    }
+}
---
+++ MuseumsManager/Entities/Museo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+    public class Museo
+    {
+        private int idMuseo;
+        private string Nome;
+        private string Luogo;
+        private TimeSpan OrarioAperturaGenerale;
+        private TimeSpan OrarioChiusuraGenerale;
+        private int NumBigliettiMaxGenerale;
+        private int idFamiglia;
+    }
+}
