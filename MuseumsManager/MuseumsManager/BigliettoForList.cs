e9430e038bd9c714420a6d87630635a8c4add3e4
Andrea <artolink97@gmail.com>
2020-12-18 21:02:18 +0100

    finished 'Biglietto'

---
+++ MuseumsManager/MuseumsManager/BigliettoForList.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MuseumsManager
+{
+    public class BigliettoForList
+    {
+        public DateTime DataValidita { get; set; }
+        public double PrezzoAcquisto { get; set; }
+        public double Prezzo { get; set; }
+        public string Nome { get; set; }
+    }
+}
